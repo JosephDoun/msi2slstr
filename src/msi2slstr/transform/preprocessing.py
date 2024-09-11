30c58781b619c9bb2b3223cee534847419b4a8ab
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-09-11 15:23:57 +0200

    docs(transform): added preprocessor `__call__` docstring

--- src/msi2slstr/transform/preprocessing.py
+++ src/msi2slstr/transform/preprocessing.py
@@ -26,6 +26,17 @@ class DataPreprocessor:
 
     def __call__(self, sen2tuple: tuple[ndarray],
                  sen3tuple: tuple[ndarray]) -> tuple[ndarray, ndarray]:
+        """
+        Executes workflow that finalizes input data for model consumption.
+
+        :param sen2tuple: A tuple of Sentinel-2 3D patches with length equal to `batch_size`
+        :type sen2tuple: tuple[ndarray]
+        :param sen3tuple: A tuple of Sentinel-3 3D patches with length equal to `batch_size`
+        :type sen3tuple: tuple[ndarray]
+
+        :return: The 4D input arrays prepared for model consuption
+        :rtype: tuple[ndarray]
+        """
         # Build batch dimension.
         sen2 = stack(sen2tuple, 0)
         sen3 = stack(sen3tuple, 0)
