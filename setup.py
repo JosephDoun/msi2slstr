bc1e92ec9a67cdc4deda6c7b10d19663c9928041
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-09-13 05:26:53 +0200

    fix: build error related to dependencies
    gdal version depends on system version found

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 import subprocess
 
 
-gdal_version = subprocess.run(["gdal-config", "--version"],
-                              capture_output=True
-                              ).stdout.decode("utf-8")
+def get_gdal_system_version():
+    return subprocess.run(["gdal-config", "--version"],
+                           capture_output=True)\
+                            .stdout\
+                                .decode("utf-8")
 
 setup(name="msi2slstr",
-      install_requires=[f"gdal=={gdal_version}",
+      install_requires=[f"gdal=={get_gdal_system_version()}",
                         "pyproj==3.6.1",
-		                "arosics==1.9.2",])
-
-
+		                "arosics==1.9.2",
+                        "onnxruntime==1.18.1"])
