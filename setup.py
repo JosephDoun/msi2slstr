48b88d80ab43a299a0fc59e9a2db80fbbeb6d3ff
Joseph Doundoulakis <67523692+JosephDoun@users.noreply.github.com>
2024-09-15 03:55:42 +0200

    * build: added conventional-changelog-cli node dependency
    closes #57
    
    * ci: added again pull_request trigger on tests.yml
    
    * build: setup.py module import bug

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup
+from importlib import util
 
+import sys
 import subprocess
+
+sys.path.append(".")
+
+import version
+
+
+spec = util.spec_from_file_location("init", "src/msi2slstr/__init__.py")
+module = util.module_from_spec(spec)
+spec.loader.exec_module(module)
 
 
 def get_gdal_system_version():
@@ -10,7 +21,9 @@ def get_gdal_system_version():
                                 .decode("utf-8")
 
 setup(name="msi2slstr",
+      version=version.__version__,
+      description=module.__doc__.strip(),
       install_requires=[f"gdal=={get_gdal_system_version()}",
                         "pyproj==3.6.1",
-		                    "arosics==1.9.2",
+                        "arosics==1.9.2",
                         "onnxruntime==1.18.1"])
