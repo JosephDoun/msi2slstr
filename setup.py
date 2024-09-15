c572000b45dc097238f76ce9a1de7e24cf010a71
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-09-15 02:35:39 +0200

    build: added conventional-changelog-cli node dependency
    closes #57

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+from  src.msi2slstr import __doc__ as description
 
 import subprocess
+import version
 
 
 def get_gdal_system_version():
@@ -10,7 +12,9 @@ def get_gdal_system_version():
                                 .decode("utf-8")
 
 setup(name="msi2slstr",
+      version=version.__version__,
+      description=description.strip(),
       install_requires=[f"gdal=={get_gdal_system_version()}",
                         "pyproj==3.6.1",
-		                "arosics==1.9.2",
+                        "arosics==1.9.2",
                         "onnxruntime==1.18.1"])
