c6694abd93de47fddf4801967dbcb1e236ce5306
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-09-18 21:31:47 +0200

    feat(evaluation): added `metrics` module

--- src/msi2slstr/evaluation/metrics.py
+++ src/msi2slstr/evaluation/metrics.py
@@ -6,13 +6,20 @@ from numpy import sqrt
 
 from typing import Self
 
+from ..transform.normalization import Standardizer
+
 
 class Pearson(ndarray):
     """
     Pearson coefficient numpy implementation.
+
+    .. math::
+        \\begin{aligned}
+            ...
+        \\end{aligned}
     """
-    def __new__(cls, __x: ndarray, __y: ndarray) -> Self:
-        coef = corrcoef(__x.flatten(), __y.flatten())
+    def __new__(cls, x: ndarray, y: ndarray) -> Self:
+        coef = corrcoef(x.flatten(), y.flatten())
         result = coef[0, 1]
         return result.view(cls)
 
@@ -20,39 +27,61 @@ class Pearson(ndarray):
 class SRMSE(ndarray):
     """
     Standardized RMSE.
+
+    .. math::
+        \\begin{aligned}
+            ...
+        \\end{aligned}
     """
-    def __new__(cls, __x: ndarray, __y: ndarray,
+    standard: Standardizer = Standardizer((-1, -2))
+    
+    def __new__(cls, x: ndarray, y: ndarray,
                 dims: tuple[int]=(-1, -2)) -> Self:
-        result = sqrt((__x - __y) ** 2).mean(axis=dims)
+        x = cls.standard(x)
+        y = cls.standard(y)
+        result = sqrt((x - y) ** 2).mean(axis=dims)
         return result.view(cls)
 
 
 class SSIM(ndarray):
     """
-    Global SSIM.
+    Global SSIM. Collapses elements along :attr:`dims` of the provided arrays 
+    to calculate the metric for the elements that remain. Defaults to a per-channel
+    computation.
+
+    :param x: First array to use in the computation of SSIM.
+    :type x: `ndarray`
+    :param y: Second array to use in the computation of SSIM.
+    :type y: `ndarray`
+    :param dims: Tuple of dimensions to collapse, defaults to (-1, -2).
+    :type dims: tuple[int] or None, optional
 
     .. math::
-        l = ...
 
-        c = ...
+        \\begin{aligned}
+            l = \\frac{2 \\bar{x} \\bar{y} + \\epsilon}{\\bar{x} ^ 2 + \\bar{y} ^ 2 + \\epsilon },\\quad&
+            c = \\frac{2 \\sigma_{x} \\sigma_{y} + \\epsilon}{\\sigma_{x} ^ 2 + \\sigma_{y} ^ 2 + \\epsilon },\\quad
+            s = \\frac{\\overline{(x - \\bar{x}) (y - \\bar{y})} + \\epsilon}{\\sigma_{x} \\sigma_{y} + \\epsilon}
+            
+            \\newline
+            \\newline
+            SSIM& = l \\times c \\times s,\quad SSIM \\in [0, 1]&
+        \\end{aligned}
 
-        s = ...
     """
     _C = 1e-10
-    def __new__(cls, __x: ndarray, __y: ndarray,
+    def __new__(cls, x: ndarray, y: ndarray,
                 dims: tuple[int]=(-1, -2)) -> Self:
-        mx = __x.mean(axis=dims, keepdims=True)
-        my = __y.mean(axis=dims, keepdims=True)
-        sx = __x. std(axis=dims, keepdims=True)
-        sy = __y. std(axis=dims, keepdims=True)
-        xnorm = __x - mx
-        ynorm = __y - my
-        sxy = (xnorm * ynorm).sum(axis=dims, keepdims=True)
-        sx2 = (xnorm * xnorm).sum(axis=dims, keepdims=True)
-        sy2 = (ynorm * ynorm).sum(axis=dims, keepdims=True)
+        mx = x.mean(axis=dims, keepdims=True)
+        my = y.mean(axis=dims, keepdims=True)
+        sx = x. std(axis=dims, keepdims=True)
+        sy = y. std(axis=dims, keepdims=True)
+        xnorm = x - mx
+        ynorm = y - my
+        sxy = (xnorm * ynorm).mean(axis=dims, keepdims=True)
         l = (2 * mx * my + cls._C) / (mx ** 2 + my ** 2 + cls._C)
         c = (2 * sx * sy + cls._C) / (sx ** 2 + sy ** 2 + cls._C)
-        s = (sxy + cls._C) / sqrt(sx2 * sy2 + cls._C)
+        s = (sxy + cls._C) / (sx * sy + cls._C)
         return (l.clip(0) *
                 c.clip(0) *
-                s.clip(0)).prod((-1, -2, -3)).view(cls)
+                s.clip(0)).prod((-1, -2)).view(cls)
