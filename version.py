ac7acd5d7e6aa82462e8350896fd8ca7902be28f
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-11-22 03:46:00 +0100

    chore: added docstring to version.py

--- version.py
+++ version.py
@@ -1,1 +1,8 @@
+"""
+Version file to dynamically pass a version number to setuptools.
+
+This file is overwritten automatically by the CI release workflow
+during release and it is not meant to be manually updated.
+"""
+
 __version__="0.0.0"
